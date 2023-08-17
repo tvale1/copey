288e1c3fe102f1229b7b34ddc78042297c4c1576
tvale1 <114479470+tvale1@users.noreply.github.com>
2023-08-17 19:48:10 +1000

    Add files via upload

---
+++ database.js
@@ -0,0 +1,26 @@
+const database = [
+    {
+        source:'vcaa',
+        year: 2022,
+        exam: 1,
+        topic: 'data analysis',
+        question_number: 2,
+        image_location: '/images/1.png'
+    },
+    {
+        source:'vcaa',
+        year: 2022,
+        exam: 1,
+        topic: 'recursion and financial modelling',
+        question_number: 17,
+        image_location: '/images/2.png'
+    },
+    {
+        source:'vcaa',
+        year: 2022,
+        exam: 1,
+        topic: 'matrices',
+        question_number: 7,
+        image_location: '/images/3.png'
+    },
+];
